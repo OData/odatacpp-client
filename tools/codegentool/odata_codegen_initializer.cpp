bffedf5c8ebe41821b711d6aa51a0162c3ff32bc
Bin Xu <bixu@microsoft.com>
2014-08-11 00:24:22 -0700

    Add license header

--- tools/codegentool/odata_codegen_initializer.cpp
+++ tools/codegentool/odata_codegen_initializer.cpp
@@ -1,4 +1,20 @@
-#include "odata_codegen_initializer.h"
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+ #include "odata_codegen_initializer.h"
 #include "odata/client/odata_client.h"
 #include "odata/edm/edm_model_utility.h"
 #include <regex>
