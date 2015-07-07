e99c82c5783b1bae02c0081ed2efcf19cd4b4413
sagnik1saha <sahasag@mit.edu>
2015-07-07 10:38:50 -0700

    Update http_implement.h
    Expose client_config object and ensure that it can be modified before it's used to construct the client

--- include/odata/communication/http_implement.h
+++ include/odata/communication/http_implement.h
@@ -20,32 +20,49 @@ namespace odata { namespace communication
 class http_client_impl
 {
 public:
-	http_client_impl(const ::utility::string_t& baseAddress, std::shared_ptr<::odata::client::odata_client_credential> credential_setting)
-	{
-		if (credential_setting)
-		{
-			::web::http::client::http_client_config client_config;
-			client_config.set_credentials(::web::http::client::credentials(credential_setting->get_username(), credential_setting->get_password()));
-			m_client = std::make_shared<::web::http::client::http_client>(::web::http::uri(baseAddress), client_config);
-		}
-		else
-		{
-            m_client = std::make_shared<::web::http::client::http_client>(::web::http::uri(baseAddress));
-		}
-		
-	}
-
-	::utility::string_t base_uri()
+    http_client_impl(const ::utility::string_t& address, std::shared_ptr<::odata::client::odata_client_credential> credential)
     {
-		return m_client->base_uri().to_string();
+        baseAddress = address;
+        credential_setting = credential;
     }
 
-	pplx::task<::web::http::http_response> send_http_request(const ::utility::string_t& method, const ::utility::string_t& request_uri, const ::utility::string_t accept);
-	pplx::task<::web::http::http_response> send_http_request(const ::utility::string_t& method, const ::utility::string_t& request_uri, const ::utility::string_t accept, ::web::json::value object);
+    ::utility::string_t base_uri()
+    {
+        return getOrCreateClient()->base_uri().to_string();
+    }
+
+    ::web::http::client::http_client_config& get_client_config(){
+        return client_config;
+    }
+
+    pplx::task<::web::http::http_response> send_http_request(const ::utility::string_t& method, const ::utility::string_t& request_uri, const ::utility::string_t accept);
+    pplx::task<::web::http::http_response> send_http_request(const ::utility::string_t& method, const ::utility::string_t& request_uri, const ::utility::string_t accept, ::web::json::value object);
 
 private:
-	std::shared_ptr<::web::http::client::http_client> m_client;
 
+    /// <summary>
+    /// If the client hasn't already been constructed, the method constructs one 
+    /// and returns it, while also caching it for future
+    /// </summary>
+    /// <Explanation>
+    /// Delays the construction of m_client until its first use
+    /// This allows others to use and modify the client_config object
+    /// (e.g. change the callback function associated with it) before
+    /// m_client is created.
+    /// </Explanation>
+    std::shared_ptr<::web::http::client::http_client>& getOrCreateClient()
+    {
+        if (m_client == nullptr)
+        {
+            if (credential_setting)  client_config.set_credentials(::web::http::client::credentials(credential_setting->get_username(), credential_setting->get_password()));
+            m_client = std::make_shared<::web::http::client::http_client>(::web::http::uri(baseAddress), client_config);
+        }
+        return m_client;
+    }
+    std::shared_ptr<::web::http::client::http_client> m_client;
+    ::utility::string_t baseAddress;
+    std::shared_ptr<::odata::client::odata_client_credential> credential_setting;
+    ::web::http::client::http_client_config client_config;
     ::web::http::http_request _build_get_request(::web::http::method method, ::web::http::uri_builder request_uri, const ::utility::string_t& accept = U("")) const;
     ::web::http::http_request _build_request(::web::http::method method, ::web::http::uri_builder request_uri, const ::utility::string_t& accept, ::web::json::value object) const;
 };
